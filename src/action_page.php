57e953e120db25cd8365db935d853845c88f9da1
EC2 Default User <ec2-user@ip-172-31-1-96.ec2.internal>
2020-04-19 00:02:35 +0000

    This update for PHP usage

--- src/action_page.php
+++ src/action_page.php
@@ -1,8 +1,47 @@
-<html>
-<body>
+require 'vendor/autoload.php';
 
-Welcome <?php echo $_POST["actname"]; ?><br>
-Your email address is: <?php echo $_POST["dicname"]; ?>
+date_default_timezone_set('UTC');
 
-</body>
-</html>
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+$sdk = new Aws\Sdk([
+    'endpoint'   => 'http://localhost:8000',
+    'region'   => 'us-east-1',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+$tableName = 'MovieArchive';
+
+$eav = $marshaler->marshalJson('
+    {
+        ":yyyy": 1981 
+    }
+');
+
+$params = [
+    'TableName' => $tableName,
+    'KeyConditionExpression' => '#Year = :yyyy',
+    'ExpressionAttributeNames'=> [ '#Year' => 'Year' ],
+    'ExpressionAttributeValues'=> $eav
+];
+
+echo "Querying for movies from 1981.\n";
+
+try {
+    $result = $dynamodb->query($params);
+
+    echo "Query succeeded.\n";
+
+    foreach ($result['Items'] as $movie) {
+        echo $marshaler->unmarshalValue($movie['year']) . ': ' .
+            $marshaler->unmarshalValue($movie['title']) . "\n";
+    }
+
+} catch (DynamoDbException $e) {
+    echo "Unable to query:\n";
+    echo $e->getMessage() . "\n";
+}

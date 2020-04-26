9f19775dfd37e9a225b89f6dc7f527fcabf64334
EC2 Default User <ec2-user@ip-172-31-1-96.ec2.internal>
2020-04-26 19:49:25 +0000

    update to code that allows the site to pull data from a text file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-console.log('starting function');
-
-const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
-
-exports.handle = function(e, ctx, callback) {
-    
-    let scanningParameters = {
-        TableName: 'MovieArchive',
-        Limit: 100
-    };
-    
-    docClient.scan(scanningParameters, function(err, data){
-        if(err){
-            callback(err, null);
-        }
-        else {
-            callback(null, data);
-        }
+/*global $*/("button").on('click', 'button', function(){
+    $.getJSON("movie-items.json", function(obj) {
+        $.each(obj, function(key, value) {
+            $("ul").append("<li>"+value.Title+"</li>");
+        });
     });
-}
+});
